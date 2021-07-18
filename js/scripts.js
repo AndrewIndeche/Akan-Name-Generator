b9220b9f2e59346a863cccafe98435982869832e
AndrewIndeche <indecheandrew@gmail.com>
2021-07-18 16:38:57 +0300

    Add Array definition names

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,9 @@
+let female_names = ["Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua",] //0-6
+let male_names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+
 function birthday(CC,YY,MM,DD){
   return ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7) - 1.5
-  //return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
+  //return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 gives wrong day of 3.5
 }
-var dayofTheWeek = birthday(19, 91, 4, 23) //0-6
+var dayofTheWeek = birthday(19, 91, 4, 23)
 console.log(dayofTheWeek)
