ba09fa87dada149b3bd76dcf5b905da1793427ba
AndrewIndeche <indecheandrew@gmail.com>
2021-07-18 14:39:47 +0300

    Add formula definition

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 function birthday(CC,YY,MM,DD){
-  return (DD+((13*(MM+1))/5)+YY+(YY/4)-(YY/100)+(YY/400)) % 7
+  return ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7) - 1.5
   //return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7
 }
-let day = birthday()
-console.log(day)
+var dayofTheWeek = birthday(19, 91, 4, 23) //0-6
+console.log(dayofTheWeek)
