f05b9e3e9bc6d8ae6689b1c326b7fedfb5674de3
AndrewIndeche <indecheandrew@gmail.com>
2021-07-21 21:12:11 +0300

    add external javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,35 +1,28 @@
-function birthday(CC,YY,MM,DD){
-  return ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7) - 1.5
-  //return ( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7 gives wrong day of 3.5
-}
-function processForm(e) {
-            e.preventDefault();
-
-var dayofTheWeek = birthday(19, 91, 4, 23)
-
-let female_names = ["Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua",] //0-6
-let male_names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-
-let gender = document.getElementById("gender").value
-let YY = parseInt(document.getElementById("YY").value)
-let CC = parseInt(document.getElementById("CC").value)
-let MM = parseInt(document.getElementById("MM").value)
-let DD = parseInt(document.getElementById("DD").value)
-
-if (dd < 0 || dd > 31) {
-       return alert( dd + " is not a valid day. The day should be between zero and 31")
-            }
-if (mm < 0 || mm > 31) {
-      return alert( mm + " is not a valid month. The month should be between zero and twelve")
-            }
-
-if (gender == "male") {
-       var name = male_names[dayOfTheWeek]
-      return alert("Your Akan name is " + name)
-            }
-
-if (gender == "female") {
-      var name = female_names[dayOfTheWeek]
-    returnalert("Your Akan name is " + name)
-            }
-console.log(dayofTheWeek)
+    let female_names = ["Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua",] //0-6
+    let male_names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
+    function getBirthday(CC, YY, MM, DD) {
+        return ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7)-1.5
+    }
+    function processForm(e) {
+        e.preventDefault();
+        let gender = document.getElementById("gender").value
+        let YY = parseInt(document.getElementById("YY").value)
+        let CC = parseInt(document.getElementById("CC").value)
+        let MM = parseInt(document.getElementById("MM").value)
+        let DD = parseInt(document.getElementById("DD").value)
+        if (DD < 0 || DD > 31) {
+            return alert( DD + " is not a valid day. The day should be between zero and 31")
+        }
+        if (MM < 0 || MM > 31) {
+            return alert( MM + " is not a valid month. The month should be between zero and twelve")
+        }
+        var dayOfTheWeek = getBirthday(CC, YY, MM, DD) //0-6
+        if (gender == "male") {
+            var name = male_names[dayOfTheWeek]
+            alert("Your Akan name is " + name)
+        }
+        if (gender == "female") {
+            var name = female_names[dayOfTheWeek]
+            alert("Your Akan name is " + name)
+        }
+    }
