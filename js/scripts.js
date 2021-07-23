7343822d2bfa71ddb019de9eacc9a36c2f1203d2
AndrewIndeche <indecheandrew@gmail.com>
2021-07-23 11:19:40 +0300

    change of parameters

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,31 @@
-    let female_names = ["Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama", "Akosua",] //0-6
+    let female_names = ['Adwoa', 'Abenaa', 'Akua', 'Yaa', 'Afua', 'Ama', 'Akosua',] //0-6
     let male_names = ['Kwasi', 'Kwadwo', 'Kwabena', 'Kwaku', 'Yaw', 'Kofi', 'Kwame']
-    function getBirthday(CC, YY, MM, DD) {
-        return ((((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7)-1.5
+    function dayofTheWeek(CC, YY, MM, DD) {
+    return new Date('' + CC + YY + '-' + MM + '-' + DD).getDay()
     }
+    var dayofTheWeek = (CC, YY, MM, DD) => new Date ('' + CC + YY + '-' + MM + '-' + DD).getDay()
+
     function processForm(e) {
         e.preventDefault();
+
         let gender = document.getElementById("gender").value
         let YY = parseInt(document.getElementById("YY").value)
         let CC = parseInt(document.getElementById("CC").value)
         let MM = parseInt(document.getElementById("MM").value)
         let DD = parseInt(document.getElementById("DD").value)
+
         if (DD < 0 || DD > 31) {
-            return alert( DD + " is not a valid day. The day should be between zero and 31")
+            return alert( DD + " is not a valid day. The day should be between 0 and 31")
         }
-        if (MM < 0 || MM > 31) {
-            return alert( MM + " is not a valid month. The month should be between zero and twelve")
+        if (MM < 0 || MM > 12) {
+            return alert( MM + " is not a valid month. The month should be between 0 and 12")
         }
-        var dayOfTheWeek = getBirthday(CC, YY, MM, DD) //0-6
         if (gender == "male") {
-            var name = male_names[dayOfTheWeek]
+            var name = male_names[dayofTheWeek(CC,YY,MM,DD)]
             alert("Your Akan name is " + name)
         }
         if (gender == "female") {
-            var name = female_names[dayOfTheWeek]
+            var name = female_names[dayofTheWeek(CC,YY,MM,DD)]
             alert("Your Akan name is " + name)
         }
     }
